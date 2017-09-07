a82cdd1df2d3f6d6bb696548a19d28e41fbf6e99
eekhin <eekhin@gmail.com>
2017-09-06 21:38:31 -0700

    Added AdactionTest class and TC101 class

---
+++ src/test/java/com/sqa/eiei/adactin/AdactinTest.java
@@ -0,0 +1,10 @@
+package com.sqa.eiei.adactin;
+
+import com.sqa.eiei.core.*;
+
+public class AdactinTest extends BasicTest {
+
+	public AdactinTest() {
+		super("http://adactin.com/HotelApp");
+	}
+}
---
+++ src/test/java/com/sqa/eiei/adactin/TC101.java
@@ -0,0 +1,13 @@
+package com.sqa.eiei.adactin;
+
+import org.testng.annotations.*;
+
+import com.sqa.eiei.adactin.*;
+
+public class TC101 extends AdactinTest {
+
+	@Test
+	public void testAdactin() throws InterruptedException {
+		Thread.sleep(10000);
+	}
+}
